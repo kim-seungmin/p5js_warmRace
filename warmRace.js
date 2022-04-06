6ca66078954c1adaa61a9cdad413789af986ed7a
kim-seungmin <54826101+kim-seungmin@users.noreply.github.com>
2022-04-06 17:54:08 +0900

    add moveSpeed fisishLine

--- warmRace.js
+++ warmRace.js
@@ -1,5 +1,6 @@
-let moveSpeed = 1;
+let moveSpeed = 0.3;
 let speedRange = 3;
+let finishLine = 900
 let posA = 5;
 let posB = 5;
 function setup() {
@@ -11,9 +12,20 @@ function draw() {
   int(random(10)%2) ? posA+=moveSpeed+random(speedRange) : posB+=moveSpeed+random(speedRange);
   console.log(random(10)%2);
   fill(255,0,0);
-  circle(posA, 100, 10)
+  circle(posA, 100, 15)
+  circle(posA-13, 100, 15)
+  circle(posA-26, 100, 15)
   
   fill(255,255,0);
-  circle(posB, 300, 10)
+  circle(posB, 300, 15)
+  circle(posB-13, 300, 15)
+  circle(posB-26, 300, 15)
+  
+  if(posB>finishLine){
+    
+  }if(posA>finishLine){
+    
+  }
+  line(finishLine,0,finishLine,height)
   
 }
